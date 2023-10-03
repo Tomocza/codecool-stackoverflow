89a7f8d1183eacf6382a65efb8c90d884489dc9a
Kozakadam <kar.adam.robert@gmail.com>
2023-10-03 08:58:56 +0200

    add navbar

---
+++ frontend/my-app/src/components/Layout/NavBar.js
@@ -0,0 +1,40 @@
+import { Outlet, Link } from "react-router-dom";
+import "./NavBar.css";
+import logoImg from "./stack-icon.svg";
+
+function NavBar(){
+ return (
+    <div className="navbar">
+        <nav>
+      <ul>
+        <li>
+          <Link to="/">
+            <button className="homePageButton">
+              <img className="logo" src={logoImg}></img><span className="mainLogoText">stack <b>overflow</b></span>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/questions">
+            <button type="button">Questions</button>
+          </Link>
+        </li>
+        {/* <li>
+        <button type="button">Products</button>
+        </li> */}
+        <li>
+        <input type="text" placeholder="Search"></input>
+        </li>
+        <li>
+        <button type="button" className="buttonLogin">Log in</button>
+        </li>
+        <li>
+        <button type="button" className="buttonSignup">Sign up</button>
+        </li>
+      </ul>
+    </nav>
+    <Outlet />
+    </div>
+ )
+}
+export default NavBar;
