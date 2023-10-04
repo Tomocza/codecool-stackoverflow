9f8491afd67911ba4ba820a91b37cf00c42d2ad8
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-04 18:07:49 +0200

    refactor login method of UserService class

--- src/main/java/com/codecool/stackoverflowtw/service/UserService.java
+++ src/main/java/com/codecool/stackoverflowtw/service/UserService.java
@@ -1,6 +1,7 @@
 package com.codecool.stackoverflowtw.service;
 
 import com.codecool.stackoverflowtw.controller.dto.user.NewUserDTO;
+import com.codecool.stackoverflowtw.controller.dto.user.SessionDTO;
 import com.codecool.stackoverflowtw.controller.dto.user.UserDTO;
 import com.codecool.stackoverflowtw.controller.dto.user.UserLoginDTO;
 import com.codecool.stackoverflowtw.dao.user.UserModel;
@@ -12,28 +13,37 @@ import org.springframework.stereotype.Service;
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
+  private static final int LOG_ROUNDS = 10;
   private final UsersDAO usersDAO;
+  private final Set<SessionDTO> activeSessions;
+  private final SecureRandom secureRandom;
   
   @Autowired
-  public UserService(UsersDAO usersDAO) {
+  public UserService(UsersDAO usersDAO, Set<SessionDTO> activeSessions, SecureRandom secureRandom) {
     this.usersDAO = usersDAO;
+    this.activeSessions = activeSessions;
+    this.secureRandom = secureRandom;
   }
   
   public List<UserDTO> getAll() {
     return usersDAO.getAll().stream().map(e -> new UserDTO(e.id(), e.username(), e.registeredAt())).toList();
   }
   
-  public Optional<UserDTO> login(UserLoginDTO userLoginDTO) {
+  public Optional<SessionDTO> login(UserLoginDTO userLoginDTO) {
     Optional<UserModel> user = usersDAO.getByName(userLoginDTO.username());
     if (user.isEmpty()) {
       return Optional.empty();
     }
     UserModel presentUser = user.get();
-    if (BCrypt.checkpw(userLoginDTO.pwd(), presentUser.pwHash())) {
-      return Optional.of(transformFromUserModel(presentUser));
+    if (BCrypt.checkpw(userLoginDTO.password(), presentUser.pwHash())) {
+      String sessionId = String.valueOf(secureRandom.nextInt());
+      SessionDTO sessionDTO = new SessionDTO(presentUser.id(), sessionId);
+      activeSessions.add(sessionDTO);
+      return Optional.of(sessionDTO);
     }
     return Optional.empty();
   }
@@ -48,7 +58,7 @@ public class UserService {
   }
   
   public int register(NewUserDTO user) {
-    String salt = BCrypt.gensalt(10, new SecureRandom());
+    String salt = BCrypt.gensalt(LOG_ROUNDS, new SecureRandom());
     String password = BCrypt.hashpw(user.password(), salt);
     NewUserDTO newUser = new NewUserDTO(user.username(), password);
     return usersDAO.add(newUser);
