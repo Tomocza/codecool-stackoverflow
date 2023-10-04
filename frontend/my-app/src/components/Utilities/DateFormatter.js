733f01174f00a4b45293a1b59643b860d5ebd4b0
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-04 22:25:53 +0200

    fix time zone issue

--- frontend/my-app/src/components/Utilities/DateFormatter.js
+++ frontend/my-app/src/components/Utilities/DateFormatter.js
@@ -1,7 +1,7 @@
 function DateFormatter({date}) {
     function formatDate() {
         let formattedDate = "";
-        let parsedDate = new Date(Date.UTC(...date));
+        let parsedDate = new Date(...date);
         formattedDate += 1900 + +parsedDate.getYear();
         formattedDate += "-" + extendToTwoDigits(parsedDate.getMonth() - 1);
         formattedDate += "-" + extendToTwoDigits(parsedDate.getDate());
