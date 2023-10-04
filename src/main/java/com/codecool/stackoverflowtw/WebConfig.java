d2b389db506dc3a6282456a34ae5ac44a3094401
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-04 15:39:45 +0200

    add WebConfig class

---
+++ src/main/java/com/codecool/stackoverflowtw/WebConfig.java
@@ -0,0 +1,30 @@
+package com.codecool.stackoverflowtw;
+
+import com.codecool.stackoverflowtw.controller.dto.user.SessionDTO;
+import com.codecool.stackoverflowtw.controller.interceptor.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+  private final Set<SessionDTO> activeSessions;
+  
+  @Autowired
+  public WebConfig(Set<SessionDTO> activeSessions) {
+    this.activeSessions = new HashSet<>(activeSessions);
+  }
+  
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new AuthenticationInterceptor(activeSessions))
+            .addPathPatterns("/**")
+            .excludePathPatterns("/users/login", "/users", "/hello", "/");
+  }
+}
