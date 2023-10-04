7b616601e557aa2500db8c4a8d1fe6bb35897165
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-05 00:59:35 +0200

    refactor SessionDTO

--- src/main/java/com/codecool/stackoverflowtw/controller/dto/user/SessionDTO.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/dto/user/SessionDTO.java
@@ -1,4 +1,9 @@
 package com.codecool.stackoverflowtw.controller.dto.user;
 
-public record SessionDTO(int user_id, String session_id) {
+import java.time.LocalDateTime;
+
+public record SessionDTO(int user_id, String session_id, LocalDateTime time_of_creation) {
+  public SessionDTO(int user_id, String session_id) {
+    this(user_id, session_id, LocalDateTime.now());
+  }
 }
