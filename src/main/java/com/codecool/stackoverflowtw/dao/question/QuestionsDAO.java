3631d9eff23d84c0c77a5284199ec9d005ef74db
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-04 10:14:30 +0200

    add ability to add and delete question votes

--- src/main/java/com/codecool/stackoverflowtw/dao/question/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/question/QuestionsDAO.java
@@ -1,6 +1,7 @@
 package com.codecool.stackoverflowtw.dao.question;
 
 import com.codecool.stackoverflowtw.controller.dto.question.NewQuestionDTO;
+import com.codecool.stackoverflowtw.controller.dto.question.QuestionVoteDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface QuestionsDAO {
   Optional<QuestionModel> getQuestionById(int id);
   int addNewQuestion(NewQuestionDTO newQuestionDTO);
   boolean deleteQuestionById(int id);
+  boolean addVoteToQuestion(QuestionVoteDTO questionVoteDTO);
+  boolean deleteQuestionVote(int questionId, int userId);
 }
--- src/main/java/com/codecool/stackoverflowtw/service/QuestionService.java
+++ src/main/java/com/codecool/stackoverflowtw/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.codecool.stackoverflowtw.service;
 import com.codecool.stackoverflowtw.controller.dto.question.BriefQuestionDTO;
 import com.codecool.stackoverflowtw.controller.dto.question.DetailedQuestionDTO;
 import com.codecool.stackoverflowtw.controller.dto.question.NewQuestionDTO;
+import com.codecool.stackoverflowtw.controller.dto.question.QuestionVoteDTO;
 import com.codecool.stackoverflowtw.dao.question.QuestionModel;
 import com.codecool.stackoverflowtw.dao.question.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.user.UserModel;
@@ -17,13 +18,13 @@ import java.util.Optional;
 public class QuestionService {
   private final QuestionsDAO questionsDAO;
   private final UsersDAO usersDAO;
-  
+
   @Autowired
   public QuestionService(QuestionsDAO questionsDAO, UsersDAO usersDAO) {
     this.questionsDAO = questionsDAO;
     this.usersDAO = usersDAO;
   }
-  
+
   public List<BriefQuestionDTO> getAllQuestions() {
     return questionsDAO.getAllQuestions()
                        .stream()
@@ -34,7 +35,7 @@ public class QuestionService {
                                                       e.answerCount()))
                        .toList();
   }
-  
+
   public Optional<DetailedQuestionDTO> getQuestionById(int id) {
     Optional<QuestionModel> result = questionsDAO.getQuestionById(id);
     return result.map(questionModel -> new DetailedQuestionDTO(questionModel.id(),
@@ -44,15 +45,23 @@ public class QuestionService {
                                                                getUsername(questionModel.user_id()),
                                                                questionModel.createdAt()));
   }
-  
+
   public boolean deleteQuestionById(int id) {
     return questionsDAO.deleteQuestionById(id);
   }
-  
+
   public int addNewQuestion(NewQuestionDTO question) {
     return questionsDAO.addNewQuestion(question);
   }
-  
+
+  public boolean addVoteToQuestion(QuestionVoteDTO questionVoteDTO) {
+    return questionsDAO.addVoteToQuestion(questionVoteDTO);
+  }
+
+  public boolean deleteQuestionVote(int questionId, int userId) {
+    return questionsDAO.deleteQuestionVote(questionId, userId);
+  }
+
   private String getUsername(int user_id) {
     return usersDAO.getById(user_id).orElseGet(() -> new UserModel(0, "anonymous", "", null)).username();
   }
