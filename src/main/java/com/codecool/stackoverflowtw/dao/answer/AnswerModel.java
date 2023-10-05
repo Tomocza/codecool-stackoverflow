ad9d0d941ffce7c8a7eb1fc8b7403581de990d56
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-05 16:33:07 +0200

    fix wrong data being sent out

--- src/main/java/com/codecool/stackoverflowtw/dao/answer/AnswerModel.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/answer/AnswerModel.java
@@ -3,5 +3,5 @@ package com.codecool.stackoverflowtw.dao.answer;
 import java.time.LocalDateTime;
 
 public record AnswerModel(int id, int questionId, String body, int userId, LocalDateTime createdAt,
-                          LocalDateTime modifiedAt, boolean accepted, int rating, boolean hasVoted) {
+                          LocalDateTime modifiedAt, boolean accepted, int rating, int hasVoted) {
 }
