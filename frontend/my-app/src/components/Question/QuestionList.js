994f06392162d396858b5b17517be711dde399b8
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-05 15:42:57 +0200

    component gets the questions from prop instead of fetching from the server

--- frontend/my-app/src/components/Question/QuestionList.js
+++ frontend/my-app/src/components/Question/QuestionList.js
@@ -1,40 +1,22 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "../../App.css";
-import Question from './Question';
-function QuestionList(){
-    const [questions, setQuestions] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-      try{
-        setLoading(true);
-        async function fetchQuestions(){
-          const response = await fetch('/questions/all');
-          const newQuestions = await response.json();
-          setQuestions(newQuestions);
-        }
-        fetchQuestions();
-      } catch(error){
-        console.error(error)
-      } finally{
-        setLoading(false)
-      }
-      }, []);
-
-    return (
-        <div className="questionList">
-            <div>
-                <h2>Top Questions</h2>
-                <Link to="/question/ask">
-                  <button className='askQuestionButton' disabled={loading}>Ask Question</button>
-                </Link>
-            </div>
-            {questions.map((question) => (
-                <Question question={question} key={question.id}/>
-            ))}
-        </div>
-    )
+import Question from "./Question";
+function QuestionList({ questions, loading }) {
+  return (
+    <div className="questionList">
+      <div>
+        <h2>Top Questions</h2>
+        <Link to="/question/ask">
+          <button className="askQuestionButton" disabled={loading}>
+            Ask Question
+          </button>
+        </Link>
+      </div>
+      {questions.map((question) => (
+        <Question question={question} key={question.id} />
+      ))}
+    </div>
+  );
 }
 export default QuestionList;
